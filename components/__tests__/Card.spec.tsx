56c7fd37a1cfe456ecff09144f8e207929c4ec8a
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-02 14:51:00 -0300

    test: ensure correct card background color

--- components/__tests__/Card.spec.tsx
+++ components/__tests__/Card.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "./setupTests";
 import { Card } from "../Card";
+import Colors from "../../constants/Colors";
 jest.mock("../../assets/images/CowIcon.png");
 const cowIcon = require("../../assets/images/CowIcon.png");
 
@@ -7,6 +8,7 @@ describe("Card", () => {
 	it("should render the component", () => {
 		render(
 			<Card
+				href="/"
 				iconSource={cowIcon}
 				testID="card-id"
 				alt="alternative text"
@@ -19,6 +21,7 @@ describe("Card", () => {
 	it("should render the title", () => {
 		render(
 			<Card
+				href="/"
 				iconSource={cowIcon}
 				alt="alternative text"
 				title="title text"
@@ -30,11 +33,70 @@ describe("Card", () => {
 	it("should render the 2 children elements", async () => {
 		render(
 			<Card
+				href="/"
 				iconSource={cowIcon}
 				alt="alternative text"
 				title="title text"
 			/>
 		);
 		expect(screen.toJSON().children.length).toBe(2);
+	});
+	it("should render green background as default", async () => {
+		render(
+			<Card
+				href="/"
+				iconSource={cowIcon}
+				alt=""
+				title="Default card"
+				testID="Default card"
+			/>
+		);
+		const cardGreen = screen.getByTestId("Default card");
+		expect(cardGreen).toHaveStyle({ backgroundColor: Colors.green });
+	});
+
+	it("should render the correct green background when color green is provided", async () => {
+		render(
+			<Card
+				href="/"
+				iconSource={cowIcon}
+				alt=""
+				title="Green card"
+				testID="Green card"
+				color="green"
+			/>
+		);
+		const cardGreen = screen.getByTestId("Green card");
+		expect(cardGreen).toHaveStyle({ backgroundColor: Colors.green });
+	});
+
+	it("should render the correct blue background when color blue is provided", async () => {
+		render(
+			<Card
+				href="/"
+				iconSource={cowIcon}
+				alt=""
+				title="Blue card"
+				testID="Blue card"
+				color="blue"
+			/>
+		);
+		const cardBlue = screen.getByTestId("Blue card");
+		expect(cardBlue).toHaveStyle({ backgroundColor: Colors.blue });
+	});
+
+	it("should render the correct purple background when color purple is provided", async () => {
+		render(
+			<Card
+				href="/"
+				iconSource={cowIcon}
+				alt=""
+				title="Purple card"
+				testID="Purple card"
+				color="purple"
+			/>
+		);
+		const cardPurple = screen.getByTestId("Purple card");
+		expect(cardPurple).toHaveStyle({ backgroundColor: Colors.purple });
 	});
 });
