e9e94aa577101654b1120369dafbb8b7e81b0946
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-02 14:52:04 -0300

    feat: add color customization on card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,36 +1,44 @@
 import React from "react";
-import {
-	Image,
-	ImageURISource,
-	StyleSheet,
-	Text,
-	View,
-	ViewProps,
-} from "react-native";
+import { Image, ImageURISource, StyleSheet, Text, View } from "react-native";
 import Colors from "../constants/Colors";
+import { Link, LinkProps } from "expo-router";
 
+type ColorOptions = "green" | "blue" | "purple";
 interface CardProps {
 	title: string;
 	alt: string;
 	iconSource: ImageURISource;
+	color?: ColorOptions;
+	href?: any;
 }
 
-export const Card: React.FC<ViewProps & CardProps> = ({
+export const Card: React.FC<LinkProps<any> & CardProps> = ({
 	title,
 	alt,
 	iconSource,
+	color = "green",
+	href,
 	...props
 }) => {
 	return (
-		<View style={styles.card} {...props}>
-			<Text style={styles.title}>{title}</Text>
-			<Image style={styles.image} source={iconSource} alt={alt} />
-		</View>
+		<Link
+			href={href}
+			style={[styles.card, { backgroundColor: Colors[color] }]}
+			{...props}
+			asChild
+		>
+			<View>
+				<Text style={styles.title}>{title}</Text>
+				<Image style={styles.image} source={iconSource} alt={alt} />
+			</View>
+		</Link>
 	);
 };
 
 const styles = StyleSheet.create({
 	card: {
+		width: "48%",
+		aspectRatio: 1.6 / 1,
 		backgroundColor: Colors.green,
 		borderRadius: 4,
 		padding: 16,
@@ -46,7 +54,7 @@ const styles = StyleSheet.create({
 		resizeMode: "contain",
 	},
 	title: {
-		fontSize: 16,
+		fontSize: 14,
 		color: Colors.white,
 	},
 });
