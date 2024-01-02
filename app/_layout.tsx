88333287d597d4ee1a1a0d7484c0453f12475670
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-02 13:28:35 -0300

    fix: remove  screens from the root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -55,7 +55,6 @@ function RootLayoutNav() {
 		>
 			<Stack>
 				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-				<Stack.Screen name="(add)" options={{ headerShown: false }} />
 			</Stack>
 		</ThemeProvider>
 	);
