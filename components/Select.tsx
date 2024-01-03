a1197199fc39e471d7f4ac0b1092bd4139b3650c
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-02 21:27:36 -0300

    feat: add select component

---
+++ components/Select.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import SelectDropdown from "react-native-select-dropdown";
+
+import { StyleSheet, Text, View, ViewProps } from "react-native";
+import Colors from "../constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+
+const dropdownIcon = () => (
+	<FontAwesome style={{ marginRight: 8 }} size={18} name="angle-down" />
+);
+
+export type Item = {
+	option: string;
+	value: string;
+};
+interface SelectProps {
+	items: Item[];
+	onSelect: (selectedItem: Item, index: number) => void;
+	error?: string;
+	label?: string;
+}
+export const Select: React.FC<SelectProps & ViewProps> = ({
+	items,
+	onSelect,
+	label,
+	error,
+	...props
+}) => {
+	const getBorderColor = () => (error ? Colors.red : Colors.border);
+	return (
+		<View>
+			{label && <Text style={styles.label}>{label}</Text>}
+			<SelectDropdown
+				renderDropdownIcon={dropdownIcon}
+				buttonTextStyle={styles.text}
+				buttonStyle={[styles.button, { borderColor: getBorderColor() }]}
+				rowStyle={styles.row}
+				rowTextStyle={styles.text}
+				dropdownStyle={styles.dropdown}
+				defaultButtonText="Selecione uma opção"
+				data={items}
+				onSelect={onSelect}
+				buttonTextAfterSelection={(selectedItem) => {
+					return selectedItem.option;
+				}}
+				rowTextForSelection={(item: Item) => {
+					return item.option;
+				}}
+				{...props}
+			/>
+			{error && <Text style={styles.error}>{error}</Text>}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	dropdown: {
+		borderRadius: 4,
+	},
+	button: {
+		borderRadius: 4,
+		backgroundColor: Colors.gray,
+		borderWidth: 1,
+		borderColor: Colors.border,
+		padding: 8,
+		marginVertical: 4,
+	},
+	row: {
+		borderRadius: 4,
+		backgroundColor: Colors.gray,
+		borderWidth: 1,
+		borderColor: Colors.border,
+		padding: 8,
+	},
+	text: {
+		fontSize: 14,
+		color: Colors.darkGray,
+	},
+	label: {
+		fontSize: 14,
+		color: Colors.darkGray,
+	},
+	error: {
+		fontSize: 12,
+		color: Colors.red,
+	},
+});
