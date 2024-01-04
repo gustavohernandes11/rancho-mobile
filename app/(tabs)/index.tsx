3bebdf5e65c6502b72b6a56ca1cb9e41d3c3dedd
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-04 14:19:44 -0300

    feat: adjust home page to display only the cards

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,100 +1,49 @@
-import { Button } from "../../components/Button";
 import { Card } from "../../components/Card";
 import { ContainerView } from "../../components/ContainerView";
 import { Heading } from "../../components/Heading";
-import { Input } from "../../components/Input";
-import { Select } from "../../components/Select";
 import { Span } from "../../components/Span";
-import { Link } from "expo-router";
-import { Item } from "../../types/Item";
-import { SimpleTable } from "../../components/SimpleTable";
-import { AnimalTable } from "../../components/AnimalTable";
-import { Animal } from "../../types/Animal";
-import { IconButton } from "../../components/IconButton";
-import { BatchInfo } from "../../components/BatchInfo";
 
 export default function TabOneScreen() {
-	const items: Item[] = [
-		{ key: "First option", value: "1" },
-		{ key: "Second option", value: "2" },
-		{ key: "Third option", value: "3" },
-		{ key: "Forthy option", value: "4" },
-	];
-	const animals: Animal[] = [
-		{ id: 1, name: "Name 1", gender: "F" },
-		{ id: 2, name: "Name 2", gender: "M" },
-		{ id: 3, name: "Name 3", gender: "M" },
-		{ id: 4, name: "Name 4", gender: "F" },
-	];
 	return (
 		<ContainerView>
 			<Heading>Início</Heading>
-			<BatchInfo
-				batch={{
-					name: "Bezerros",
-					description: "Bezerros de 1 a 2 anos de idade",
-					count: 23,
-				}}
-			/>
-			<Span>
-				<IconButton icon="expand" />
-				<IconButton type="danger" icon="trash" />
-				<IconButton type="light" icon="search" />
-			</Span>
-			<AnimalTable animals={animals} />
-			<SimpleTable data={items} />
-			<Span>
-				<Select
-					label="Lote"
-					error="Campo requerido"
-					items={items}
-					onSelect={() => {}}
-				/>
-			</Span>
-			<Span>
-				<Input label="Idade" textArea={true} />
-			</Span>
-			<Span flexWrap="nowrap" alignItems="flex-start">
-				<Input label="Idade" placeholder="Um input qualquer" />
-				<Input label="Idade" error="Algum erro" />
-			</Span>
-			<Span paddingVertical={8} flexWrap="wrap">
+
+			<Span flexWrap="wrap">
 				<Card
-					href="/"
+					href="/add-animal"
 					alt="alt"
 					iconSource={require("../../assets/images/CowIcon.png")}
 					title="Adicionar animal"
 				/>
 				<Card
-					href="/"
+					href="/view-animals"
 					alt="alt"
 					iconSource={require("../../assets/images/CowIcon.png")}
 					title="Rebanho"
 				/>
 				<Card
-					href="/"
+					href="/add-batch"
 					alt="alt"
 					iconSource={require("../../assets/images/FenceIcon.png")}
 					title="Adicionar lote"
 					color="blue"
 				/>
 				<Card
-					href="/"
+					href="/view-batches"
 					alt="alt"
 					iconSource={require("../../assets/images/FenceIcon.png")}
 					title="Ver lotes"
 					color="blue"
 				/>
-
 				<Card
-					href="/"
+					href="/add-production"
 					alt="alt"
 					iconSource={require("../../assets/images/GallonIcon.png")}
 					title="Adicionar produção"
 					color="purple"
 				/>
 				<Card
-					href="/"
+					href="/view-production"
 					alt="alt"
 					iconSource={require("../../assets/images/GallonIcon.png")}
 					title="Ver produção"
