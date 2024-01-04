e37a28a3b91e711bebbd37e3b09e06cd0f790db9
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-04 11:24:29 -0300

    test: ensure BatchInfo renders and with the correct grammatical flexion

---
+++ components/__tests__/BatchInfo.spec.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "./setupTests";
+
+import { BatchInfo } from "../BatchInfo";
+
+describe("BatchInfo", () => {
+	const mockedBatch = {
+		name: "Bezerros",
+		description: "Bezerros de 1 a 2 anos de idade",
+		count: 23,
+	};
+	it("should render the batch information on the screen", () => {
+		render(<BatchInfo batch={mockedBatch} />);
+		expect(screen.getByText("Bezerros")).toBeOnTheScreen();
+		expect(
+			screen.getByText("Bezerros de 1 a 2 anos de idade")
+		).toBeOnTheScreen();
+		expect(screen.getByText("23 animais")).toBeOnTheScreen();
+	});
+	it("should render the grammatical flexion 'animal' [pt-BR] when count is 1", () => {
+		render(
+			<BatchInfo batch={{ name: "any", description: "any", count: 1 }} />
+		);
+		expect(screen.getByText("1 animal")).toBeOnTheScreen();
+	});
+	it("should render the grammatical flexion 'animais' [pt-BR] when count is bigger then 0", () => {
+		render(
+			<BatchInfo
+				batch={{ name: "any", description: "any", count: 123 }}
+			/>
+		);
+		expect(screen.getByText("123 animais")).toBeOnTheScreen();
+	});
+});
