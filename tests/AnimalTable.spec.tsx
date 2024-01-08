4dd0533fdaae535b49e38018d84304531fba9295
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-08 16:45:34 -0300

    fix: incompatible types

--- tests/AnimalTable.spec.tsx
+++ tests/AnimalTable.spec.tsx
@@ -6,10 +6,10 @@ jest.useFakeTimers();
 
 describe("AnimalTable", () => {
 	const mockedAnimals: Animal[] = [
-		{ id: 1, name: "name-1", gender: "F" },
-		{ id: 2, name: "name-2", gender: "M" },
-		{ id: 3, name: "name-3", gender: "M" },
-		{ id: 4, name: "name-4", gender: "F" },
+		{ id: "1", name: "name-1", gender: "F" },
+		{ id: "2", name: "name-2", gender: "M" },
+		{ id: "3", name: "name-3", gender: "M" },
+		{ id: "4", name: "name-4", gender: "F" },
 	];
 	it("should render all the animals", () => {
 		render(<AnimalTable animals={mockedAnimals} />);
