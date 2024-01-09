144f9afc18b1966043645357ee48e31c12079fbe
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-09 17:29:16 -0300

    feat: finish screen to view batch details

--- app/(screens)/batches/[id].tsx
+++ app/(screens)/batches/[id].tsx
@@ -1,11 +1,14 @@
 import { ContainerView } from "../../../components/ContainerView";
 import { createStorageService } from "../../../database/createStorageServiceFactory";
 import { useEffect, useState } from "react";
-import { Text } from "react-native";
-import { Span } from "../../../components/Span";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { Batch } from "../../../types/Batch";
 import { Animal } from "../../../types/Animal";
+import { AnimalTable } from "../../../components/AnimalTable";
+import { Heading } from "../../../components/Heading";
+import { HelperText } from "react-native-paper";
+import { SimpleTable } from "../../../components/SimpleTable";
+import { Item } from "../../../types/Item";
 
 export default function ViewBatchDetailsScreen() {
 	const { id } = useLocalSearchParams<{ id: string }>();
@@ -25,10 +28,30 @@ export default function ViewBatchDetailsScreen() {
 
 	return (
 		<ContainerView>
-			<Stack.Screen options={{ headerTitle: batch?.name }} />
-			<Span>
-				<Text>Detalhes do lote {batch?.name}</Text>
-			</Span>
+			<Stack.Screen options={{ headerTitle: "Detalhes do lote" }} />
+			<Heading>{batch?.name}</Heading>
+			<HelperText style={{ padding: 0, marginBottom: 8 }} type="info">
+				{`Detalhes do lote "${batch?.name}"`}
+			</HelperText>
+			<Heading size="small">Informações Gerais</Heading>
+			<SimpleTable data={serializeBatchInfoToKeyValue(batch)} />
+			<Heading size="small">Animais</Heading>
+			<AnimalTable animals={animals || []} />
 		</ContainerView>
 	);
 }
+
+const serializeBatchInfoToKeyValue = (batch?: Batch) => {
+	let items: Item[] = [];
+	if (!batch) return items;
+
+	if (batch.description)
+		items.push({ key: "Descrição", value: batch.description });
+	if (batch.count)
+		items.push({
+			key: "Número de animais",
+			value: batch?.count.toString(),
+		});
+
+	return items;
+};
