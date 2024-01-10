6271af7d042d84155ca93f7bd58d732debfc1736
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-10 17:10:50 -0300

    chore: remove mock

--- app/(screens)/animals/add.tsx
+++ app/(screens)/animals/add.tsx
@@ -1,13 +1,12 @@
 import { Stack } from "expo-router";
 import { ContainerView } from "../../../components/ContainerView";
 import { AnimalForm } from "../../../components/AnimalForm";
-import { mockedAnimals } from "../../../database/mockedRepository/mockedAnimals";
 
 export default function AddAnimalScreen() {
 	return (
 		<ContainerView>
 			<Stack.Screen options={{ headerTitle: "Adicionar animal" }} />
-			<AnimalForm initialValues={mockedAnimals[0]} />
+			<AnimalForm />
 		</ContainerView>
 	);
 }
