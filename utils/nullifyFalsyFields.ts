86167e208aba5a15564d595abac495d03ef54e41
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-16 15:29:26 -0300

    fix(nullifyFalsyFields): clone method

--- utils/nullifyFalsyFields.ts
+++ utils/nullifyFalsyFields.ts
@@ -1,5 +1,5 @@
 export const nullifyFalsyFields = (obj: any) => {
-	const clone = structuredClone(obj);
+	const clone = { ...obj };
 	for (const key in clone) {
 		if (clone.hasOwnProperty(key) && !clone[key]) {
 			clone[key] = null;
