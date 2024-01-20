8c1dc0e120e69a790e9999b656fc078d02fe050f
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-20 13:48:03 -0300

    refactor: group up components tests

---
+++ tests/utils/getFormattedAge.spec.ts
@@ -0,0 +1,5 @@
+describe("getFormattedAge", () => {
+	it("should return the days, if the age is less than a month", () => {});
+	it("should return the months, if the age is less than a year", () => {});
+	it("should return the years and months, if the age is bigger than a year", () => {});
+});
