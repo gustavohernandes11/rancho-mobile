116bffb5ea60aa61e2ce9710cf573b143d0ed1bc
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-23 11:58:04 -0300

    fix: incorrect filtering for paternity or maternity

--- utils/filters.ts
+++ utils/filters.ts
@@ -1,36 +1,38 @@
 import moment from "moment";
 import { Animal } from "types/Animal";
 
+const atLeastOneYearOld = (animal: Animal) =>
+	animal.birthdate ? moment().diff(animal.birthdate, "years") > 1 : true;
+const oneYearOlderThenChild = (animal: Animal, childBirthdate?: string) =>
+	animal.birthdate
+		? moment(animal.birthdate).isBefore(
+				moment(childBirthdate).subtract(1, "years")
+		  )
+		: true;
+
+const isMale = (animal: Animal) => animal.gender === "F";
+const isFemale = (animal: Animal) => animal.gender === "M";
+
 export const filterPossibleMaternity = (
 	animals: Animal[],
 	childBirthdate?: string
 ) => {
-	return (
-		animals
-			?.filter((a) => a.gender === "F")
-			?.filter((a) =>
-				childBirthdate
-					? moment(a.birthdate).isBefore(
-							moment(childBirthdate).add(1, "y")
-					  )
-					: true
-			) || []
-	);
+	let filteredAnimals = animals
+		.filter(isFemale)
+		.filter(atLeastOneYearOld)
+		.filter((animal) => oneYearOlderThenChild(animal, childBirthdate));
+
+	return filteredAnimals;
 };
 
 export const filterPossiblePaternity = (
 	animals: Animal[],
 	childBirthdate?: string
 ) => {
-	return (
-		animals
-			?.filter((a) => a.gender === "M")
-			?.filter((a) =>
-				childBirthdate
-					? moment(a.birthdate).isBefore(
-							moment(childBirthdate).add(1, "y")
-					  )
-					: true
-			) || []
-	);
+	let filteredAnimals = animals
+		.filter(isMale)
+		.filter(atLeastOneYearOld)
+		.filter((animal) => oneYearOlderThenChild(animal, childBirthdate));
+
+	return filteredAnimals;
 };
