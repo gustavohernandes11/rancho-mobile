ee06c8dfc1db110f42ac4d5de159396ea213b64f
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-04-05 20:39:17 -0300

    test: AddButton component basic tests

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -1,18 +1,23 @@
 import Colors from "constants/Colors";
 import { Link } from "expo-router";
-import { IconButton } from "react-native-paper";
+import { IconButton, IconButtonProps } from "react-native-paper";
 
 type AddButtonProps = {
 	href: any;
 };
-export const AddButton = ({ href }: AddButtonProps) => {
+export const AddButton = ({
+	href,
+	...props
+}: AddButtonProps & Omit<IconButtonProps, "icon">) => {
 	return (
 		<Link href={href} asChild>
 			<IconButton
 				size={24}
 				iconColor={Colors.white}
-				icon={require("../assets/images/AddAnimalIcon.png")}
+				accessibilityLabel="Add animal icon"
 				style={{ marginRight: 11, marginLeft: 0 }}
+				{...props}
+				icon={require("../assets/images/AddAnimalIcon.png")}
 			/>
 		</Link>
 	);
