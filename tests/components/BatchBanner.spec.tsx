11d2d3a94a939e6fb6bf0a2b5da983f9a07c550b
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-04-05 20:53:41 -0300

    test(BatchBanner): ensure it indicate empty array when count is 0

--- tests/components/BatchBanner.spec.tsx
+++ tests/components/BatchBanner.spec.tsx
@@ -2,36 +2,8 @@ import { BatchBanner } from "components/BatchBanner";
 import { render, screen } from "tests/setupTests";
 
 describe("BatchBanner", () => {
-	const mockedBatch = {
-		id: 1,
-		name: "fake_name",
-		description: "fake_description",
-		count: 10,
-	};
-
 	it("should render the batch information on the screen", () => {
-		render(<BatchBanner batch={mockedBatch} />);
-		expect(screen.getByText("fake_name")).toBeOnTheScreen();
-		expect(screen.getByText("fake_description")).toBeOnTheScreen();
-		expect(screen.getByText("10 animais")).toBeOnTheScreen();
-	});
-
-	it("should render the singular grammatical flexion when count is 1", () => {
-		render(
-			<BatchBanner
-				batch={{
-					id: 1,
-					name: "fake_name",
-					description: "fake_description",
-					count: 1,
-				}}
-			/>
-		);
-		expect(screen.getByText("1 animal")).toBeOnTheScreen();
-	});
-
-	it("should render the plural grammatical flexion when count is bigger then 0", () => {
-		render(
+		const { getByText } = render(
 			<BatchBanner
 				batch={{
 					id: 1,
@@ -41,6 +13,64 @@ describe("BatchBanner", () => {
 				}}
 			/>
 		);
-		expect(screen.getByText("10 animais")).toBeOnTheScreen();
+
+		const name = getByText("fake_name");
+		const description = getByText("fake_description");
+		const count = getByText("10 animais");
+
+		expect(name).toBeOnTheScreen();
+		expect(description).toBeOnTheScreen();
+		expect(count).toBeOnTheScreen();
+	});
+
+	it("should render the singular grammatical flexion when count is 1", () => {
+		const { getByText } = render(
+			<BatchBanner
+				batch={{
+					id: 1,
+					name: "fake_name",
+					description: "fake_description",
+					count: 1,
+				}}
+			/>
+		);
+
+		const count = getByText("1 animal");
+
+		expect(count).toBeOnTheScreen();
+	});
+
+	it("should render 'Vazio' when count is 0", () => {
+		const { getByText } = render(
+			<BatchBanner
+				batch={{
+					id: 1,
+					name: "fake_name",
+					description: "fake_description",
+					count: 0,
+				}}
+			/>
+		);
+
+		const count = getByText("Vazio");
+
+		expect(count).toBeOnTheScreen();
+	});
+
+	it("should render the plural grammatical flexion when count is bigger then 0", () => {
+		const { getByText } = render(
+			<BatchBanner
+				batch={{
+					id: 1,
+					name: "fake_name",
+					description: "fake_description",
+					count: 10,
+				}}
+			/>
+		);
+
+		const count = getByText("10 animais");
+
+		expect(count).toBeOnTheScreen();
 	});
 });
