0d135d2b00e35bd8e5f160171b93429298be3142
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-04-07 09:41:24 -0300

    chore(GoBackButton): add accessibility label

--- components/GoBackButton.tsx
+++ components/GoBackButton.tsx
@@ -1,21 +1,25 @@
 import Colors from "constants/Colors";
 import { useNavigation } from "expo-router";
 import React from "react";
-import { IconButton } from "react-native-paper";
+import { IconButton, IconButtonProps } from "react-native-paper";
 
 interface GoBackButtonProps {
 	children?: React.ReactNode;
 }
 
-export const GoBackButton: React.FC<GoBackButtonProps> = () => {
+export const GoBackButton: React.FC<
+	GoBackButtonProps & Omit<IconButtonProps, "icon">
+> = ({ ...props }) => {
 	const navigation = useNavigation();
 	return (
 		<IconButton
 			size={24}
 			iconColor={Colors.white}
-			icon="arrow-left"
+			accessibilityLabel="Go back"
 			style={{ marginRight: 11, marginLeft: 0 }}
 			onPress={navigation.goBack}
+			icon="arrow-left"
+			{...props}
 		/>
 	);
 };
