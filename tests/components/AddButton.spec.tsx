677d5476d3a3334280378db0ee58cd127e46d333
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-06-19 11:27:40 -0300

    fix(AddButton.spec.tsx): add icon property

--- tests/components/AddButton.spec.tsx
+++ tests/components/AddButton.spec.tsx
@@ -5,14 +5,22 @@ jest.useFakeTimers().setSystemTime(new Date("2024-01-20"));
 describe("AnimalBanner", () => {
 	it("should render the button on the screen", () => {
 		const { getByTestId } = render(
-			<AddButton href="dummy_href" testID="add_animal_icon_button" />
+			<AddButton
+				icon="add-animal"
+				href="dummy_href"
+				testID="add_animal_icon_button"
+			/>
 		);
 		const button = getByTestId("add_animal_icon_button");
 		expect(button).toBeOnTheScreen();
 	});
 	it("should render the icon", () => {
 		const { getByLabelText } = render(
-			<AddButton href="dummy_href" testID="add_animal_icon_button" />
+			<AddButton
+				icon="add-animal"
+				href="dummy_href"
+				testID="add_animal_icon_button"
+			/>
 		);
 		const button = getByLabelText("Add animal icon");
 		expect(button).toBeOnTheScreen();
