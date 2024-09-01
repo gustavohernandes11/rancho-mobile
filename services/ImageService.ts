77ecc462dd1e27c576b89faa34198cde43386050
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-09-01 13:45:09 -0300

    refactor(imageServices): decrease animal image quality

--- services/ImageService.ts
+++ services/ImageService.ts
@@ -35,7 +35,6 @@ export class ImageServices implements ImageServiceMethods {
             aspect: [1, 1],
             mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
-            quality: 1,
         });
 
         if (pickerResult.canceled || pickerResult.assets.length === 0) {
